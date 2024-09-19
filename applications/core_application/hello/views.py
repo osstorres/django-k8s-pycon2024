947a39773a4a6d1e67578466b8e8b19a18e0c189
osieltorres <osieltorresdev@gmail.com>
2024-09-18 20:33:20 -0600

    CHORE: App load

--- applications/core_application/hello/views.py
+++ applications/core_application/hello/views.py
@@ -1,8 +1,11 @@
 from rest_framework import status, permissions, serializers
 from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
 import socket
-
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import time
+import threading
 
 class EmptySerializer(serializers.Serializer):
     pass
@@ -15,5 +18,28 @@ class HostView(GenericAPIView):
     def get(self, request):
         return Response(
             f"Hello from core application v1 :: {socket.gethostname()}",
+            status=status.HTTP_200_OK,
+        )
+
+def cpu_intensive_task(duration):
+    start_time = time.time()
+    while True:
+        elapsed_time = time.time() - start_time
+        if elapsed_time > duration:
+            break
+        # Busy-wait loop
+        start_busy = time.time()
+        while time.time() - start_busy < 0.1:  # Busy-wait for 0.1 seconds
+            pass
+        # Sleep to reduce CPU usage
+        time.sleep(0.15)  # Sleep for 0.15 seconds
+
+class LoadView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        threading.Thread(target=cpu_intensive_task, args=(60,)).start()
+        return Response(
+            "OK",
             status=status.HTTP_200_OK,
         )
